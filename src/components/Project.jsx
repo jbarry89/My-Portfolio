6f772e1e9b9ab2ee7a18d7434540225d84cbc1d3
jbarry89 <joelbarry8907@hotmail.com>
2025-01-27 12:45:27 -0500

    Modified code

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import '../styles/Project.css';
 
-export default function Project({title, image, appLink, repoLink}) {
+export default function Project({title, image, liveURL, gitHubURL}) {
     return (
-        <div>
+        <div className='project-card'>
+            <img src={image} alt={`${title} screenshot`} className='project-image' />
             <h3>{title}</h3>
-            <img src={image} alt={`${title} screenshot`} />
-            <div>
-             <a href={appLink} target="_blank" rel="noopener noreferrer">Deployed App</a>
-             <a href={repoLink} target="_blank" rel='noopener noreferrer'>GitHub Repo</a>
+            <div className='icons'>
+                <a href={liveURL} target="_blank" rel="noopener noreferrer" className='icon'>
+                    <i className='fas fa-external-link-alt'></i> Live Demo
+                </a>
+             <a href={gitHubURL} target="_blank" rel='noopener noreferrer' className='icon'>
+                <i className='fab fa-github'></i> GitHub</a>
             </div>
         </div>
     );
-
 }
