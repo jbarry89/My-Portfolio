d4639de5347ef11afc19ff1c9bcb1796cc58d5af
jbarry89 <joelbarry8907@hotmail.com>
2025-05-29 01:42:27 -0400

    Modified About Me page.

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -5,10 +5,8 @@ export default function AboutMe() {
   return (
     <section className="about-me-section">
       <div className='about-me-content'>
-        <h2>About Me</h2>
-        <h3>Hi! I'm <span className='text-color'>Joel</span></h3>
-        <p>A passionate web developer with a love for creating visually appealing 
-          and functional web experiences. My journey into web development began with a fascination 
+        <h2 className="fade-in">About Me</h2>
+        <p className="fade-in">My journey into web development began with a fascination 
           for how design and technology combine to create meaningful user interactions. While I 
           enjoy exploring all aspects of development, I’m especially enthusiastic about web design 
           and front-end development, where I can unleash my creativity and enhance user experiences 
