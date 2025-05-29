779d47ba988f7a2391bb487a94bd34859e55de3f
jbarry89 <joelbarry8907@hotmail.com>
2025-05-29 01:44:10 -0400

    Modified the Resume page.

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -4,23 +4,24 @@ export default function Resume(){
     return(
         <section className='resume-section'>
             <h2>Resume</h2>
-            <a href="/resume-template.pdf" download>Download Resume</a>
+            <a href="/joel-resume-web-Dev.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
             <h3> Front-End Proficiencies</h3>
             <ul>
-                <li>HTML</li>
+                <li>HTML5</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
+                <li>Tailwind CSS, Bootstrap, Chakra UI</li>
                 <li>SQL Query</li>
                 <li>React</li>
-                <li>Bootstrap</li>
-                <li>JavaScript</li>
 
             </ul>
             <h3> Back-End Proficiencies</h3>
             <ul>
                 <li>NodeJS</li>
                 <li>ExpressJS</li>
-                <li>API's</li>
+                <li>RESTful APIs</li>
+                <li>MongoDB</li>
+                <li>JWT Authentication</li>
             </ul>
             
         </section>
