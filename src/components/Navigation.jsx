e3fe579028a2cca01cbc50939982f342dd1d7f59
jbarry89 <joelbarry8907@hotmail.com>
2025-05-29 01:41:27 -0400

    re-assign url of about me page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ export default function Navigation() {
             <div className='container py-2 flex md:justify-center md: justify-between items-center'>
                 <div className= 'text-2xl font-bold'>Joel Barry</div>
                 <div className= 'space-x-6'>
-                    <li><Link to="/">About Me</Link></li>
+                    <li><Link to="/aboutme">About Me</Link></li>
                     <li><Link to="/portfolio">Portfolio</Link></li>
                     <li><Link to="/contact">Contact Me</Link></li>
                     <li><Link to="/resume">Resume</Link></li>
